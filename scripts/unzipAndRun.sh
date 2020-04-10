870eb11362e455cd6fe97ba92ca50c440b236d6c
Rado Buransky <radoburansky@gmail.com>
2020-04-11 00:04:12 +0200

    Akka init

---
+++ scripts/unzipAndRun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo rm -rf ~/ostrostroj/ostrostroj-app-1.0-SNAPSHOT
+unzip ~/ostrostroj/ostrostroj-app-1.0-SNAPSHOT.zip -d ~/ostrostroj
+sudo ~/ostrostroj/ostrostroj-app-1.0-SNAPSHOT/bin/ostrostroj-app
+
