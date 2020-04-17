469af8a5822fb7ed0595616e5bb765b9515b23d2
Rado Buransky <radoburansky@gmail.com>
2020-04-17 09:44:29 +0200

    New way is working finally

--- ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/controller/LedMatrix.scala
+++ ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/controller/LedMatrix.scala
@@ -19,31 +19,34 @@ object LedMatrix {
         Behaviors.same
       case Test =>
         // Turn off and then on
-        sendToAllChips(oldMax7219, 0x0C, 0x00)
-        sendToAllChips(oldMax7219, 0x0C, 0x01)
+        sendToAllChips(oldMax7219, driver, 0x0C, 0x00)
+        sendToAllChips(oldMax7219, driver, 0x0C, 0x01)
 
         // Turn off test mode
-        sendToAllChips(oldMax7219, 0x0F, 0x01)
-        sendToAllChips(oldMax7219, 0x0F, 0x00)
+        sendToAllChips(oldMax7219, driver, 0x0F, 0x01)
+        sendToAllChips(oldMax7219, driver, 0x0F, 0x00)
         // Set decode mode (no decoding)
-        sendToAllChips(oldMax7219, 0x09, 0x00)
+        sendToAllChips(oldMax7219, driver, 0x09, 0x00)
         // Set scan limit (8 segments)
-        sendToAllChips(oldMax7219, 0x0B, 0x07)
+        sendToAllChips(oldMax7219, driver, 0x0B, 0x07)
         // Set min intensity
-        sendToAllChips(oldMax7219, 0x0A, 0x00)
+        sendToAllChips(oldMax7219, driver, 0x0A, 0x00)
 
         // Old way
-        oldMax7219 ! OldWord(0x01, 0x01, 0)
-        oldMax7219 ! OldWord(0x02, 0x02, 0)
-        oldMax7219 ! OldWord(0x03, 0x04, 0)
-        oldMax7219 ! OldWord(0x04, 0x08, 0)
-        oldMax7219 ! OldWord(0x05, 0x10, 0)
-        oldMax7219 ! OldWord(0x06, 0x20, 0)
-        oldMax7219 ! OldWord(0x07, 0x40, 0)
-        oldMax7219 ! OldWord(0x08, 0x80.toByte, 0)
+//        oldMax7219 ! OldWord(0x01, 0x01, 0)
+//        oldMax7219 ! OldWord(0x02, 0x02, 0)
+//        oldMax7219 ! OldWord(0x03, 0x04, 0)
+//        oldMax7219 ! OldWord(0x04, 0x08, 0)
+//        oldMax7219 ! OldWord(0x05, 0x10, 0)
+//        oldMax7219 ! OldWord(0x06, 0x20, 0)
+//        oldMax7219 ! OldWord(0x07, 0x40, 0)
+//        oldMax7219 ! OldWord(0x08, 0x80.toByte, 0)
 
-        // Show some data
-//        drawSlash(driver, 0)
+        // New way
+        drawSlash(driver, 0)
+        drawSlash(driver, 1)
+        drawSlash(driver, 2)
+        drawSlash(driver, 3)
 
 //        max7219 ! Max7219.Word(0x04, 0xFF.toByte, 1)
 //
@@ -73,19 +76,24 @@ object LedMatrix {
   }
 
   private def drawSlash(driver: ActorRef[DriverCommand], chipIndex: Int): Unit = {
-//    driver ! Word(0x01, 0x01, chipIndex)
-//    driver ! Word(0x02, 0x02, chipIndex)
-//    driver ! Word(0x03, 0x04, chipIndex)
-//    driver ! Word(0x04, 0x08, chipIndex)
-//    driver ! Word(0x05, 0x10, chipIndex)
-//    driver ! Word(0x06, 0x20, chipIndex)
-//    driver ! Word(0x07, 0x40, chipIndex)
-//    driver ! Word(0x08, 0x80.toByte, chipIndex)    
+    driver ! Word(0x01, 0x01, chipIndex)
+    driver ! Word(0x02, 0x02, chipIndex)
+    driver ! Word(0x03, 0x04, chipIndex)
+    driver ! Word(0x04, 0x08, chipIndex)
+    driver ! Word(0x05, 0x10, chipIndex)
+    driver ! Word(0x06, 0x20, chipIndex)
+    driver ! Word(0x07, 0x40, chipIndex)
+    driver ! Word(0x08, 0x80.toByte, chipIndex)
   }
 
-  private def sendToAllChips(oldMax7219: ActorRef[OldMax7219.Command], address: Byte, data: Byte): Unit = {
+  private def sendToAllChips(oldMax7219: ActorRef[OldMax7219.Command],
+                             driver: ActorRef[DriverCommand], address: Byte, data: Byte): Unit = {
     for (i <- 0 to 3) {
-      oldMax7219 ! OldWord(address, data, i)
+      // Old way:
+      // oldMax7219 ! OldWord(address, data, i)
+
+      // New way:
+      driver ! Word(address, data, i)
     }
   }
 }
