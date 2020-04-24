fb18acb0ee1be3f509f171f401fa612c8c0b0ff5
Rado Buransky <radoburansky@gmail.com>
2020-04-24 13:18:18 +0200

    Text writer unit tested.

--- ostrostroj-app/src/test/scala/com/buransky/ostrostroj/app/controls/display/TextWriterSpec.scala
+++ ostrostroj-app/src/test/scala/com/buransky/ostrostroj/app/controls/display/TextWriterSpec.scala
@@ -9,23 +9,64 @@ import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TextWriterSpec extends AnyFlatSpec {
+  import TextWriterSpec._
+
   behavior of "write"
 
-  it should "write abc" in {
+  it should "write \"a\"" in {
     // Prepare
+    val expected =
+      """..........-
+        |..........-
+        |..........-
+        |...XX.....-
+        |..X.X.....-
+        |...XX.....-""".stripMargin
     val ledMatrix = mock(classOf[LedMatrix])
-    val textWriter = new TextWriter(ledMatrix, 8)
+    val textWriter = new TextWriter(ledMatrix, totalRowCount)
 
     // Execute
     textWriter.write("a", Position(2, 2), color = true)
 
     // Verify
-    verify(ledMatrix).setLedStatus(4, 3, true)
-    verify(ledMatrix).setLedStatus(4, 4, true)
-    verify(ledMatrix).setLedStatus(3, 2, true)
-    verify(ledMatrix).setLedStatus(3, 4, true)
-    verify(ledMatrix).setLedStatus(2, 3, true)
-    verify(ledMatrix).setLedStatus(2, 4, true)
+    verifyExpectedLedStatus(expected, color = true, ledMatrix)
     verifyNoMoreInteractions(ledMatrix)
   }
+
+  it should "write \"čh7\"" in {
+    // Prepare
+    val expected =
+      """XXX..X.XX.-
+        |..X..X...X-
+        |.XX..XXX.X-
+        |.X...X.X.X-""".stripMargin
+    val ledMatrix = mock(classOf[LedMatrix])
+    val textWriter = new TextWriter(ledMatrix, totalRowCount)
+
+    // Execute
+    textWriter.write("čh7", Position(0, 0), color = false)
+
+    // Verify
+    verifyExpectedLedStatus(expected, color = false, ledMatrix)
+    verifyNoMoreInteractions(ledMatrix)
+  }
+
+  private def verifyExpectedLedStatus(expected: String, color: Boolean, ledMatrix: LedMatrix): Unit = {
+    var row = totalRowCount - 1
+    var column = 0
+    expected.foreach {
+      case '.' => column += 1
+      case 'X' =>
+        verify(ledMatrix).setLedStatus(row, column, color)
+        column += 1
+      case '-' =>
+        row -= 1
+        column = 0
+      case _ =>
+    }
+  }
+}
+
+object TextWriterSpec {
+  val totalRowCount = 8
 }
