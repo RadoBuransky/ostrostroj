ece2626a2a64b35dca57bf51d04f89fa0026dad4
Rado Buransky <radoburansky@gmail.com>
2020-04-27 13:46:59 +0200

    Open HiFi Shield 2 audio mixer

--- ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/AudioPlayer.scala
+++ ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/AudioPlayer.scala
@@ -3,14 +3,16 @@ package com.buransky.ostrostroj.app.audio
 import java.nio.file.Path
 
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
-import akka.actor.typed.{ActorRef, Behavior}
-import javax.sound.sampled.{AudioSystem, SourceDataLine}
+import akka.actor.typed.{ActorRef, Behavior, PostStop, Signal}
+import com.buransky.ostrostroj.app.common.OstrostrojException
+import javax.sound.sampled.{AudioSystem, Mixer}
 import org.slf4j.LoggerFactory
 
 /**
  * Multi-track audio player capable of looping, track un/muting and software down-mixing.
  */
 object AudioPlayer {
+  private val HIFI_SHIELD_2 = "ODROIDDAC"
   private val logger = LoggerFactory.getLogger(AudioPlayer.getClass)
 
   final case class Position(sample: Int)
@@ -37,19 +39,32 @@ object AudioPlayer {
 
     if (logger.isDebugEnabled) {
       AudioSystem.getMixerInfo.foreach { mixerInfo =>
-        logger.debug(s"${mixerInfo.getName}, ${mixerInfo.getVendor}, ${mixerInfo.getDescription}")
+        logger.debug(s"Name: ${mixerInfo.getName}")
+        logger.debug(s"Vendor: ${mixerInfo.getVendor}")
+        logger.debug(s"Description: ${mixerInfo.getDescription}")
+
         val mixer = AudioSystem.getMixer(mixerInfo)
-
-        val targetLinesInfo = mixer.getTargetLineInfo.map(_.toString)
-          .mkString("Target lines:" + System.lineSeparator(), System.lineSeparator(), "")
-        logger.debug(targetLinesInfo)
-
         val sourceLinesInfo = mixer.getSourceLineInfo.map(_.toString)
           .mkString("Source lines:" + System.lineSeparator(), System.lineSeparator(), "")
         logger.debug(sourceLinesInfo)
       }
     }
 
+    private val mixer: Mixer = AudioSystem.getMixer(getHifiShieldMixerInfo)
+    logger.info("Audio mixer open.")
+
+    private def getHifiShieldMixerInfo: Mixer.Info = {
+      AudioSystem.getMixerInfo.find(_.getName.contains(HIFI_SHIELD_2))
+        .getOrElse(throw new OstrostrojException("Audio mixer for HiFi shield not found!"))
+    }
+
     override def onMessage(msg: Command): Behavior[Command] = Behaviors.ignore
+
+    override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+      case PostStop =>
+        mixer.close()
+        logger.info("Audio mixer closed.")
+        Behaviors.same
+    }
   }
 }
