b17c05cb45c14fbf12e313094d6802911f7b97fe
Rado Buransky <radoburansky@gmail.com>
2020-05-28 10:48:15 +0200

    Separate sync and async (thread) input

--- ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/PlaylistInput.scala
+++ ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/PlaylistInput.scala
@@ -1,11 +1,12 @@
 package com.buransky.ostrostroj.app.audio
 
-import com.buransky.ostrostroj.app.audio.impl.ListeningPlaylistInputThread
+import com.buransky.ostrostroj.app.audio.impl.AsyncListeningPlaylistInput
 
-private[audio] trait PlaylistInput {
+private[audio] trait PlaylistInput extends AutoCloseable {
+  def run(): Unit
 }
 
 private[audio] object PlaylistInput {
-  def apply(initSong: Int, javaSoundOutput: JavaSoundOutput): PlaylistInput = new ListeningPlaylistInputThread(initSong,
+  def apply(initSong: Int, javaSoundOutput: JavaSoundOutput): PlaylistInput = new AsyncListeningPlaylistInput(initSong,
     javaSoundOutput)
 }
---
+++ ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/impl/SyncListeningPlaylistInput.scala
@@ -0,0 +1,67 @@
+package com.buransky.ostrostroj.app.audio.impl
+
+import com.buransky.ostrostroj.app.audio._
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.annotation.tailrec
+
+private[audio] class AsyncListeningPlaylistInput(initSong: Int, javaSoundOutput: JavaSoundOutput)
+  extends SyncListeningPlaylistInput(initSong, javaSoundOutput) { self =>
+
+  import SyncListeningPlaylistInput._
+
+  private val thread = new Thread {
+    override def run(): Unit = self.run()
+  }
+
+  @tailrec
+  final override def run(): Unit = {
+    try {
+      super.run()
+    }
+    catch {
+      case _: InterruptedException =>
+        logger.info(s"Playlist input thread interrupted by InterruptedException.")
+      case t: Throwable =>
+        logger.error("Playlist input thread failed!", t)
+        throw t
+    }
+    if (!thread.isInterrupted) {
+      run()
+    } else {
+      logger.info(s"Playlist input thread stopped because it was interrupted.")
+    }
+  }
+
+  override def close(): Unit = {
+    thread.interrupt()
+  }
+}
+
+private[audio] class SyncListeningPlaylistInput(initSong: Int, javaSoundOutput: JavaSoundOutput)
+  extends PlaylistInput {
+  import SyncListeningPlaylistInput._
+
+  override def run(): Unit = {
+    logger.trace("Acquiring semaphore for empty buffers...")
+    javaSoundOutput.emptyAvailable.acquire()
+    javaSoundOutput.nextEmpty() match {
+      case Some(buffer) =>
+        logger.trace("Empty buffer dequeued.")
+
+        val lastBufferEnd = javaSoundOutput.bufferingPosition.getOrElse(PlaybackPosition(initSong, SampleCount(0)))
+        // TODO: Fill the buffer
+        val audioBuffer: AudioBuffer = ???
+
+        javaSoundOutput.write(audioBuffer)
+      case None =>
+        logger.warn("Semaphore acquired but no empty buffer in the queue.")
+    }
+  }
+
+  override def close(): Unit = {}
+}
+
+private object SyncListeningPlaylistInput {
+  val logger: Logger = LoggerFactory.getLogger(classOf[SyncListeningPlaylistInput])
+}
