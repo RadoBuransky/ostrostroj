2cc536c343461b596de770cafecdc4ee1d200dd7
Rado Buransky <radoburansky@gmail.com>
2020-06-03 17:33:07 +0200

    AudioBufferSpec

--- ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/AudioBuffer.scala
+++ ostrostroj-app/src/main/scala/com/buransky/ostrostroj/app/audio/AudioBuffer.scala
@@ -29,28 +29,45 @@ case class AudioBuffer(byteArray: Array[Byte],
                        position: FrameCount,
                        limit: FrameCount,
                        endOfStream: Boolean) {
-  checkArgument(byteArray.length > 0)
-  checkArgument(frameSize == 2 || frameSize == 4)
-  checkArgument(position.value >= 0)
-  checkArgument(limit.value >= 0)
-  checkArgument(limit.value >= position.value)
+  checkArgument(byteArray.length > 0, "byteArray is empty.".asInstanceOf[AnyRef])
+  checkArgument(frameSize == 2 || frameSize == 4, s"Unsupported frameSize. [$frameSize]".asInstanceOf[AnyRef])
+  checkArgument(position.value >= 0, s"Negative position. [${position.value}]".asInstanceOf[AnyRef])
+  checkArgument(limit.value >= 0, s"Negative limit. [${limit.value}]".asInstanceOf[AnyRef])
+  checkArgument(limit.value >= position.value, (s"Limit smaller than position. " +
+    s"[${position.value}, ${limit.value}]").asInstanceOf[AnyRef])
+  checkArgument(position.value < capacity.value, (s"Position outside of byteBuffer. " +
+    s"[${position.value}, ${capacity.value}]").asInstanceOf[AnyRef])
+  checkArgument(limit.value <= capacity.value, (s"Limit outside of byteBuffer. " +
+    s"[${limit.value}, ${capacity.value}]").asInstanceOf[AnyRef])
 
   def size: FrameCount = FrameCount(limit.value - position.value)
   def capacity: FrameCount = FrameCount(byteArray.length / frameSize)
   def bytePosition: Int = position.value*frameSize
-  def byteLimit: Int = limit.value*frameSize
-  def byteSize: Int = byteLimit - bytePosition
+  def byteLimit: Int = limit.value*frameSize+1
+  def byteSize: Int = (limit-position).value*frameSize
   def byteCapacity: Int = byteArray.length
 }
 
 object AudioBuffer {
   private val logger = LoggerFactory.getLogger(classOf[AudioBuffer])
 
+  /**
+   * Creates empty audio buffer.
+   * @param audioFormat Audio format of the buffer.
+   * @param byteSize Byte size of the buffer. Must be aligned to frame size of the format.
+   */
   def apply(audioFormat: AudioFormat, byteSize: Int): AudioBuffer = {
+    checkArgument(byteSize%audioFormat.getFrameSize == 0, (s"Illegal byteSize. " +
+      s"[$byteSize, ${audioFormat.getFrameSize}]").asInstanceOf[AnyRef])
     val byteArray = new Array[Byte](byteSize)
-    new AudioBuffer(byteArray, audioFormat.getFrameSize, FrameCount(0), FrameCount(0), endOfStream = true)
+    new AudioBuffer(byteArray, audioFormat.getFrameSize, FrameCount(0), FrameCount(0), endOfStream = false)
   }
 
+  /**
+   * Load the whole audio file located at the provided path to audio buffer.
+   * @param path Audio file to load.
+   * @param audioFileReader Reader to use to load the file.
+   */
   def apply(path: Path, audioFileReader: AudioFileReader): AudioBuffer = {
     val audioInputStream = audioFileReader.getAudioInputStream(path.toFile)
     try {
