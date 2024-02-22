073d3c992ae5901f20f08c09fd1e22f4c8f33685
Rado Buransky <radoburansky@gmail.com>
2024-02-22 09:14:40 +0100

    Shutdown works

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
+#include <unistd.h>
 #include <iostream>
+#include <sys/reboot.h>
+#include <string.h>
 
-int main(int, char**){
-    std::cout << "Hello, from ostrostroj!\n";
+int main(int argc, char* argv[]){
+    if ((argc > 1) && (strcmp(argv[1], "shutdown") == 0)) {
+        sync();
+        reboot(RB_POWER_OFF); // Requires sudo
+        std::cout << "Shutdown!\n";
+    } else {
+        std::cout << "Hello, from ostrostroj!\n";
+    }
 }
