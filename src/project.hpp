16bf2689c0a7ebd227f1b18f906c8f108f6af339
Rado Buransky <radoburansky@gmail.com>
2024-03-13 22:03:11 +0100

    ...

--- src/project.hpp
+++ src/project.hpp
@@ -1,15 +1,37 @@
 #pragma once
 
 #include <vector>
+#include <map>
 #include "sample.hpp"
 
 struct Program {
     private:
-        std::vector<Sample> loops;
-        std::vector<Sample> shots;
+        int start_number;
+        std::vector<LoopSample> loops;
+        std::map<uint8_t, OneShotSample> one_shots;
 };
 
 class Project {
     private:
         std::vector<Program> programs;
+
+    public:
+        Project(std::filesystem::path dir);
+        ~Project();
+};
+
+class ProjectStatus {
+    private:
+        Project project;
+        int active_program_number;
+        std::vector<SampleReader> loop_readers;
+        std::vector<SampleReader> one_shot_readers;
+
+    public:
+        ProjectStatus(Project project);
+        ~ProjectStatus();
+
+        // TODO: Load all loops and one-shots
+        void set_program(int program_number);
+        void play_one_shot(uint8_t note);
 };
--- src/sample.hpp
+++ src/sample.hpp
@@ -8,7 +8,7 @@
 
 struct Buffer {
     public:
-        double samples[BUFFER_LEN];
+        float samples[BUFFER_LEN];
         int count;
 };
 
@@ -33,13 +33,39 @@ class Sample {
 
 class SampleReader {
     private:
-        const Sample sample;
-        const bool loop;
+        Sample sample;
+        bool loop;
         std::forward_list<Buffer>::const_iterator it;
 
         void reset();
     public:
-        SampleReader(std::filesystem::path path, bool loop);
+        SampleReader(Sample sample, bool loop);
+        // TODO: Unload sample
+        ~SampleReader();
 
-        int read(double buffer[], int count);
+        int read(std::vector<std::vector<float>> channelBuffers, int count);
+        int get_samplerate();
+        int get_format();
+};
+
+class LoopSample {
+    private:
+        Sample sample;
+        int track;
+
+    public:
+        LoopSample(std::filesystem::path path);
+        ~LoopSample();
+        SampleReader createReader();
+};
+
+class OneShotSample {
+    private:
+        Sample sample;
+        uint8_t note;
+
+    public:
+        OneShotSample(std::filesystem::path path);
+        ~OneShotSample();
+        SampleReader createReader();
 };
