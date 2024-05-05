59da92a75dea4eeddb56b77a55662c69ddf87be4
Rado Buransky <radoburansky@gmail.com>
2024-05-05 10:34:20 +0200

    read more bytes at once

--- src/alsamidi.cpp
+++ src/alsamidi.cpp
@@ -14,69 +14,48 @@ static void* run_thru(void* context) {
     }
     snd_seq_event_t event;
     std::array<unsigned char, 64> decoded;
-    unsigned char* decoded_current = decoded.data();
-    timespec prev_event_clock = {0,0};
-    timespec current_event_clock = {0,0};
-    timespec clock_wait = {0,0};
-    int log_count = 0;
-    clock_gettime(CLOCK_MONOTONIC, &prev_event_clock);
+    unsigned char* decoded_current;
     SPDLOG_INFO("ALSA rawmidi thru started.");
     while (!self.stop) {
-        snd_rawmidi_read(self.handle_in, decoded_current, 1);
-        res = snd_midi_event_encode_byte(parser, *decoded_current, &event);
-        if (res < 0) {
-            SPDLOG_ERROR("snd_midi_event_encode_byte failed = {}", res);
-            snd_midi_event_reset_encode(parser);
-            decoded_current = decoded.data();
-        } else {
-            decoded_current++;
-            if (res == 1) {
-                bool pass = true;
-                switch (event.type) {
-                    case SND_SEQ_EVENT_START:
-                        SPDLOG_INFO("SND_SEQ_EVENT_START");
-                        break;
-                    case SND_SEQ_EVENT_CONTINUE:
-                        SPDLOG_INFO("SND_SEQ_EVENT_CONTINUE");
-                        break;
-                    case SND_SEQ_EVENT_STOP:
-                        SPDLOG_INFO("SND_SEQ_EVENT_STOP");
-                        break;
-                    case SND_SEQ_EVENT_PGMCHANGE:                        
-                        SPDLOG_INFO("SND_SEQ_EVENT_PGMCHANGE [ch={},param={},value={}]", event.data.control.channel,
-                            event.data.control.param, event.data.control.value);
-                        pass = ((event.data.control.value % 2) == 0);
-                        break;
-                    case SND_SEQ_EVENT_CLOCK:
-                        if (clock_gettime(CLOCK_MONOTONIC, &current_event_clock) != 0) {
-                            SPDLOG_ERROR("clock_gettime failed");
-                        }
-                        clock_wait.tv_nsec = (current_event_clock.tv_nsec - prev_event_clock.tv_nsec) +
-                            ((current_event_clock.tv_sec - prev_event_clock.tv_sec) * 1000000000L) - 1000000L;
-                        if (clock_wait.tv_nsec > 0) {
-                            log_count++;
-                            if (log_count % 100 == 0) {
-                                SPDLOG_INFO("wait = {} s + {} ns", clock_wait.tv_sec, clock_wait.tv_nsec);
-                            }
-                            prev_event_clock = current_event_clock;
-                            nanosleep(&clock_wait, nullptr);
-                        } else {
-                            SPDLOG_WARN("Negative wait {} [{}:{} - {}:{}]" , clock_wait.tv_nsec, current_event_clock.tv_sec, current_event_clock.tv_nsec,
-                                prev_event_clock.tv_sec, prev_event_clock.tv_nsec);
-                            prev_event_clock = current_event_clock;
-                        }
-                        break;
-                    default:
-                        SPDLOG_TRACE("thru: 0x{:x}", ch);
-                        break;
-                }
-                if (pass) {
-                    snd_rawmidi_write(self.handle_out, decoded.data(), decoded_current - decoded.data());
-                    snd_rawmidi_drain(self.handle_out);
-                }
+        decoded_current = decoded.data();
+        ssize_t read_size = snd_rawmidi_read(self.handle_in, decoded.data(), decoded.size());
+        while (read_size > 0) {
+            int event_encode_res = snd_midi_event_encode(parser, decoded_current, read_size, &event);
+            if (event_encode_res < 0) {
+                SPDLOG_ERROR("snd_midi_event_encode_byte failed = {}", event_encode_res);
+                snd_midi_event_reset_encode(parser);
                 decoded_current = decoded.data();
+                read_size = 0;
             } else {
-                if (decoded_current >= decoded.data() + decoded.size()) {
+                if (event.type != SND_SEQ_EVENT_NONE) {
+                    bool pass = true;
+                    switch (event.type) {
+                        case SND_SEQ_EVENT_START:
+                            SPDLOG_INFO("SND_SEQ_EVENT_START");
+                            break;
+                        case SND_SEQ_EVENT_CONTINUE:
+                            SPDLOG_INFO("SND_SEQ_EVENT_CONTINUE");
+                            break;
+                        case SND_SEQ_EVENT_STOP:
+                            SPDLOG_INFO("SND_SEQ_EVENT_STOP");
+                            break;
+                        case SND_SEQ_EVENT_PGMCHANGE:                        
+                            SPDLOG_INFO("SND_SEQ_EVENT_PGMCHANGE [ch={},param={},value={}]", event.data.control.channel,
+                                event.data.control.param, event.data.control.value);
+                            pass = ((event.data.control.value % 2) == 0);
+                            break;
+                        default:
+                            SPDLOG_TRACE("thru: 0x{:x}", ch);
+                            break;
+                    }
+                    if (pass) {
+                        snd_rawmidi_write(self.handle_out, decoded_current, event_encode_res);
+                        snd_rawmidi_drain(self.handle_out);
+                    }
+                }
+                decoded_current += event_encode_res;
+                read_size -= event_encode_res;
+                if (read_size > 0 && decoded_current >= decoded.end()) {
                     SPDLOG_ERROR("Decoded buffer overflow!");
                     decoded_current = decoded.data();
                 }
@@ -98,13 +77,12 @@ snd_rawmidi_t* AlsaMidi::open_in(const std::string& device_name) {
     snd_rawmidi_params_t *params;
     snd_rawmidi_params_malloc(&params);
     snd_rawmidi_params_current(result, params);
-    size_t old_buffer_size = snd_rawmidi_params_get_buffer_size(params);
-    SPDLOG_INFO("Old buffer size = {}", old_buffer_size);
-    // snd_rawmidi_params_set_buffer_size(result, params, 64*1024);
-    err = snd_rawmidi_params(result, params);  
-    if (err) {
-        SPDLOG_ERROR("snd_rawmidi_params {} failed: {}", device_name, err);
-    }
+    size_t avail_min = snd_rawmidi_params_get_avail_min(params);
+    SPDLOG_INFO("ALSA rawmidi params [avail_min={}]", avail_min);
+    // err = snd_rawmidi_params(result, params);  
+    // if (err) {
+    //     SPDLOG_ERROR("snd_rawmidi_params {} failed: {}", device_name, err);
+    // }
     snd_rawmidi_params_free(params);
     SPDLOG_INFO("ALSA rawmidi input open. [{}]", device_name);
     return result;
