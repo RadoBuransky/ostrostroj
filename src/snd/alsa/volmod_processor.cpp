bc2341283a35e72a55b98ea067f592c78c841b02
Rado Buransky <radoburansky@gmail.com>
2025-02-08 14:40:00 +0100

    ...

--- src/snd/alsa/volmod_processor.cpp
+++ src/snd/alsa/volmod_processor.cpp
@@ -5,9 +5,135 @@
 
 static const std::vector<snd_seq_event_t> EMPTY_RESULT = {};
 
-VolModProcessor::VolModProcessor() {    
+// Input
+static const uint8_t CHANNEL = 16;
+static const uint8_t MW_MOD_CONTROL_CC = 83;
+static const uint8_t BC_MOD_CONTROL_CC = 84;
+static const uint8_t PB_MOD_CONTROL_CC = 85;
+static const uint8_t AT_MOD_CONTROL_CC = 86;
+static const std::array<uint8_t, VOLMOD_MOD_COUNT> MOD_CONTROL_CC = {
+    MW_MOD_CONTROL_CC,
+    BC_MOD_CONTROL_CC,
+    PB_MOD_CONTROL_CC,
+    AT_MOD_CONTROL_CC };
+static const uint8_t BASE_VOLUME_CONTROL_CC = 87;
+static const std::array<uint8_t, VOLMOD_TRACK_COUNT> VOLUME_CONTROL_CC = {
+    BASE_VOLUME_CONTROL_CC,
+    BASE_VOLUME_CONTROL_CC + 1,
+    BASE_VOLUME_CONTROL_CC + 2,
+    BASE_VOLUME_CONTROL_CC + 3};
+
+// Output
+static const uint8_t MW_CC = 1;
+static const uint8_t BC_CC = 2;
+// TODO: Aftertouch command 0xA0
+// TODO: Pitch bend command 0xE0
+static const uint8_t ELEKTRON_TRACK_LEVEL_CC = 95;
+static const std::array<uint8_t, VOLMOD_TRACK_COUNT> ELEKTRON_TRACK_CHANNELS = {1, 2, 3, 4};
+
+std::vector<uint8_t> VolModProcessor::getSelectedMods(uint8_t messageMod) {
+    std::vector<uint8_t> selectedMods = {};
+    for (uint8_t mod = 0; mod < VOLMOD_MOD_COUNT; mod++) {
+        if (modSelected.at(mod)) {
+            selectedMods.push_back(MOD_CONTROL_CC.at(mod));
+        }
+    }
+    if (selectedMods.empty()) {
+        selectedMods.push_back(MOD_CONTROL_CC.at(messageMod));
+    }
+    return selectedMods;
+}
+
+std::vector<uint8_t> VolModProcessor::getSelectedChannels(uint8_t messageMod) {
+    std::vector<uint8_t> selectedTracks = {};
+    for (uint8_t track = 0; track < VOLMOD_TRACK_COUNT; track++) {
+        if (trackSelected.at(track)) {
+            selectedTracks.push_back(ELEKTRON_TRACK_CHANNELS.at(track));
+        }
+    }
+    if (selectedTracks.empty()) {
+        selectedTracks.push_back(ELEKTRON_TRACK_CHANNELS.at(messageMod));
+    }
+    return selectedTracks;
+}
+
+snd_seq_event_t VolModProcessor::createEvent(uint8_t mod, uint8_t channel, int value) {
+    snd_seq_event_t result = {};
+    switch (mod) {
+        case BASE_VOLUME_CONTROL_CC:
+            result.type = SND_SEQ_EVENT_CONTROLLER;
+            result.data.control.channel = channel;
+            result.data.control.param = ELEKTRON_TRACK_LEVEL_CC;
+            result.data.control.value = value;
+            break;
+        case MW_MOD_CONTROL_CC:
+            result.type = SND_SEQ_EVENT_CONTROLLER;
+            result.data.control.channel = channel; 
+            result.data.control.param = MW_CC;
+            result.data.control.value = value;
+            break;
+        case BC_MOD_CONTROL_CC:
+            result.type = SND_SEQ_EVENT_CONTROLLER;
+            result.data.control.channel = channel; 
+            result.data.control.param = BC_CC;
+            result.data.control.value = value;
+            break;
+    }
+    return result;
+}
+
+std::vector<snd_seq_event_t> VolModProcessor::selectedController(std::vector<uint8_t> selectedMods, std::vector<uint8_t> selectedChannels, int value) {
+    std::vector<snd_seq_event_t> result = {};
+    for (uint8_t mod = 0; mod < selectedMods.size(); mod++) {
+        for (uint8_t channel = 0; channel < selectedChannels.size(); channel++) {
+            snd_seq_event_t event = createEvent(selectedMods.at(mod), selectedChannels.at(channel), value);
+            result.push_back(event);
+        }
+    }
+    return result;
+}
+
+std::vector<snd_seq_event_t> VolModProcessor::note(snd_seq_ev_note_t noteEvent) {
+    if (noteEvent.channel != CHANNEL) {
+        return EMPTY_RESULT;
+    }
+    return EMPTY_RESULT;
+}
+
+std::vector<snd_seq_event_t> VolModProcessor::controller(snd_seq_ev_ctrl_t controllerEvent) {
+    std::vector<snd_seq_event_t> result;
+    if (controllerEvent.channel != CHANNEL) {
+        return EMPTY_RESULT;
+    }
+    for (uint8_t i = 0; i < VOLMOD_TRACK_COUNT; i++) {
+        if (controllerEvent.param == VOLUME_CONTROL_CC.at(i)) {
+            result.push_back(createEvent(BASE_VOLUME_CONTROL_CC, ELEKTRON_TRACK_CHANNELS.at(i), controllerEvent.value));
+            return result;
+        }
+    }
+    for (uint8_t mod = 0; mod < VOLMOD_MOD_COUNT - 1; mod++) {
+        if (controllerEvent.param == MOD_CONTROL_CC.at(mod)) {
+            std::vector<uint8_t> selectedMods = getSelectedMods(mod);
+            std::vector<uint8_t> selectedChannels = getSelectedChannels(mod);
+            return selectedController(selectedMods, selectedChannels, controllerEvent.value);
+        }
+    }
+    return EMPTY_RESULT;
+}
+
+VolModProcessor::VolModProcessor():
+    trackSelected() {    
 }
 
 std::vector<snd_seq_event_t> VolModProcessor::process(snd_seq_event_t &event) {
-    return EMPTY_RESULT;
+    switch (event.type) {
+        case SND_SEQ_EVENT_NOTEON:
+            return note(event.data.note);
+        case SND_SEQ_EVENT_NOTEOFF:
+            return note(event.data.note);
+        case SND_SEQ_EVENT_CONTROLLER:
+            return controller(event.data.control);
+        default:
+            return EMPTY_RESULT;
+    }    
 }
--- src/snd/alsa/volmod_processor.hpp
+++ src/snd/alsa/volmod_processor.hpp
@@ -1,6 +1,18 @@
 #pragma once
 
+static const uint8_t VOLMOD_TRACK_COUNT = 4;
+static const uint8_t VOLMOD_MOD_COUNT = 4;
+
 class VolModProcessor {
+    private:
+        std::array<bool, VOLMOD_TRACK_COUNT> trackSelected;
+        std::array<bool, VOLMOD_MOD_COUNT> modSelected;
+        std::vector<uint8_t> getSelectedMods(uint8_t messageMod);
+        std::vector<uint8_t> getSelectedChannels(uint8_t messageMod);
+        snd_seq_event_t createEvent(uint8_t mod, uint8_t channel, int value);
+        std::vector<snd_seq_event_t> selectedController(std::vector<uint8_t> selectedMods, std::vector<uint8_t> selectedTracks, int value);
+        std::vector<snd_seq_event_t> note(snd_seq_ev_note_t noteEvent);
+        std::vector<snd_seq_event_t> controller(snd_seq_ev_ctrl_t controllerEvent);
     public:
         VolModProcessor();
         virtual ~VolModProcessor() = default;
